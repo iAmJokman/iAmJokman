3dc665e0f49c0e0eded578fc886d177166221a73
Jose Manjarres <iamjokman1987@gmail.com>
2022-05-24 02:39:57 -0500

    Las operaciones quedan funcionales

--- main.js
+++ main.js
@@ -1,24 +1,111 @@
+let displayValue = 0;
+let savedValue1 = 0;
+let savedValue2 = 0;
+let savedOperator = 0;
 
-function concatSeven(){
-    input = document.getElementById("keyboardSeven")
-    value = input.value
-    displayOperation = document.getElementById("inputOperation")
-    if (displayOperation.innerText == ''){
-        displayOperation.innerText = value
-    }else{
-        displayOperation.innerText = displayOperation.innerText + value
+function concat(value){
+    if (savedValue1 == 0){
+        displayOperation = document.getElementById("inputOperation")
+        if (displayOperation.innerText == ''){
+            displayOperation.innerText = value
+            displayValue = displayOperation.innerText
+        }else{
+            displayOperation.innerText = displayOperation.innerText + value
+            displayValue = displayOperation.innerText
+        }
+    }else if (savedValue1 != 0 && displayValue==0){
+        displayOperation.innerText = ''
+        displayOperation = document.getElementById("inputOperation")
+        if (displayOperation.innerText == ''){
+            displayOperation.innerText = value
+            displayValue = displayOperation.innerText
+        }else{
+            displayOperation.innerText = displayOperation.innerText + value
+            displayValue = displayOperation.innerText
+        }
+    }else if (savedValue1 != 0 && displayValue != 0){
+        displayOperation = document.getElementById("inputOperation")
+        if (displayOperation.innerText == ''){
+            displayOperation.innerText = value
+            displayValue = displayOperation.innerText
+        }else{
+            displayOperation.innerText = displayOperation.innerText + value
+            displayValue = displayOperation.innerText
+        }
     }
 
 }
 
-function concatSix(){
-    input = document.getElementById("keyboardSix")
-    value = input.innerText
-    displayOperation = document.getElementById("inputOperation")
-    if (displayOperation.innerText == ''){
-        displayOperation.innerText = value
-    }else{
-        displayOperation.innerText = displayOperation.innerText + value
+
+
+function operation(operator){
+    savedOperator = operator
+    savedValue1 = displayValue
+    displayValue = 0
+}
+
+function sum(){
+    savedOperator = "+"
+    savedValue1 = displayValue
+}
+
+function rest(){
+    savedOperator = "-"
+    savedValue1 = displayValue
+}
+
+function divide(){
+    savedOperator = "/"
+    savedValue1 = displayValue
+}
+
+function percentage(){
+    savedOperator = "%"
+    savedValue1 = displayValue
+}
+
+function equals(){
+    savedValue2 = displayValue
+    switch(savedOperator){
+        case "*":
+            displayOperation = document.getElementById("inputOperation")
+            displayOperation.innerText = savedValue1*savedValue2
+            displayValue = displayOperation.innerText
+            break;
+        
+        case "+":
+            displayOperation = document.getElementById("inputOperation")
+            displayOperation.innerText = number(savedValue1)+number(savedValue2)
+            displayValue = displayOperation.innerText
+            break;
+        
+        case "-":
+            displayOperation = document.getElementById("inputOperation")
+            displayOperation.innerText = savedValue1-savedValue2
+            displayValue = displayOperation.innerText
+            break;
+        
+        case "/":
+            displayOperation = document.getElementById("inputOperation")
+            displayOperation.innerText = savedValue1/savedValue2
+            displayValue = displayOperation.innerText
+            break;
+        
+        case "%":
+            displayOperation = document.getElementById("inputOperation")
+            displayOperation.innerText = (savedValue1*savedValue2)/100
+            displayValue = displayOperation.innerText
+            break;
+
     }
+}
+
+function clear(){
+    displayOperation = document.getElementById("inputOperation")
+    displayOperation.innerText = ""
+    displayValue = 0;
+    savedValue1 = 0;
+    savedValue2 = 0;
+    savedOperator = 0;
 
 }
