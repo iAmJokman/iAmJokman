0b2c46e0c2a3ebe447d7fe7233e32797a25028ef
Jose Manjarres <iamjokman1987@gmail.com>
2022-06-10 22:00:19 -0500

    Se realiza diseño basico de calculadora

--- main.js
+++ main.js
@@ -2,41 +2,43 @@ let displayValue = 0;
 let savedValue1 = 0;
 let savedValue2 = 0;
 let savedOperator = 0;
+input = document.getElementById("inputOperation");
 
 function concat(value){
     if (savedValue1 == 0){
-        displayOperation = document.getElementById("inputOperation")
-        if (displayOperation.innerText == ''){
-            displayOperation.innerText = value
-            displayValue = displayOperation.innerText
+        if (input.innerText == 0 ){
+            if(value == '.' || input.innerText == '0.'){
+                input.innerText = input.innerText + value
+                displayValue = input.innerText
+            }else{
+                input.innerText = value
+                displayValue = input.innerText
+            }
+            
         }else{
-            displayOperation.innerText = displayOperation.innerText + value
-            displayValue = displayOperation.innerText
+            input.innerText = input.innerText + value
+            displayValue = input.innerText
         }
     }else if (savedValue1 != 0 && displayValue==0){
-        displayOperation.innerText = ''
-        displayOperation = document.getElementById("inputOperation")
-        if (displayOperation.innerText == ''){
-            displayOperation.innerText = value
-            displayValue = displayOperation.innerText
+        input.innerText = ''
+        if (input.innerText == ''){
+            input.innerText = value
+            displayValue = input.innerText
         }else{
-            displayOperation.innerText = displayOperation.innerText + value
-            displayValue = displayOperation.innerText
+            input.innerText = input.innerText + value
+            displayValue = input.innerText
         }
     }else if (savedValue1 != 0 && displayValue != 0){
-        displayOperation = document.getElementById("inputOperation")
-        if (displayOperation.innerText == ''){
-            displayOperation.innerText = value
-            displayValue = displayOperation.innerText
+        if (input.innerText == ''){
+            input.innerText = value
+            displayValue = input.innerText
         }else{
-            displayOperation.innerText = displayOperation.innerText + value
-            displayValue = displayOperation.innerText
+            input.innerText = input.innerText + value
+            displayValue = input.innerText
         }
     }
 
 }
-
-
 
 function operation(operator){
     savedOperator = operator
@@ -44,68 +46,41 @@ function operation(operator){
     displayValue = 0
 }
 
-function sum(){
-    savedOperator = "+"
-    savedValue1 = displayValue
-}
-
-function rest(){
-    savedOperator = "-"
-    savedValue1 = displayValue
-}
-
-function divide(){
-    savedOperator = "/"
-    savedValue1 = displayValue
-}
-
-function percentage(){
-    savedOperator = "%"
-    savedValue1 = displayValue
-}
-
 function equals(){
     savedValue2 = displayValue
     switch(savedOperator){
         case "*":
-            displayOperation = document.getElementById("inputOperation")
-            displayOperation.innerText = savedValue1*savedValue2
-            displayValue = displayOperation.innerText
+            input.innerText = savedValue1*savedValue2
+            displayValue = input.innerText
             break;
         
         case "+":
-            displayOperation = document.getElementById("inputOperation")
-            displayOperation.innerText = number(savedValue1)+number(savedValue2)
-            displayValue = displayOperation.innerText
+            input.innerText = Number(savedValue1)+Number(savedValue2)
+            displayValue = input.innerText
             break;
         
         case "-":
-            displayOperation = document.getElementById("inputOperation")
-            displayOperation.innerText = savedValue1-savedValue2
-            displayValue = displayOperation.innerText
+            input.innerText = savedValue1-savedValue2
+            displayValue = input.innerText
             break;
         
         case "/":
-            displayOperation = document.getElementById("inputOperation")
-            displayOperation.innerText = savedValue1/savedValue2
-            displayValue = displayOperation.innerText
+            input.innerText = savedValue1/savedValue2
+            displayValue = input.innerText
             break;
         
         case "%":
-            displayOperation = document.getElementById("inputOperation")
-            displayOperation.innerText = (savedValue1*savedValue2)/100
-            displayValue = displayOperation.innerText
+            input.innerText = (savedValue1*savedValue2)/100
+            displayValue = input.innerText
             break;
 
     }
 }
 
-function clear(){
-    displayOperation = document.getElementById("inputOperation")
-    displayOperation.innerText = ""
-    displayValue = 0;
-    savedValue1 = 0;
-    savedValue2 = 0;
-    savedOperator = 0;
+function deleteDisplay(){
+    input.innerText = 0 
+    savedValue1 = 0
+    savedValue2 = 0
+    savedOperator = 0
 
 }
